3bc96b71a35c6cef1b0334f24eee86f31cfda179
Aziz Fadouach <afadouac@e1r12p9.1337.ma>
2025-01-17 16:00:56 +0100

    mofassa push

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,59 @@
 import { useState } from 'react'
-import { Calculator, Plus, Divide, Minus, X } from 'lucide-react'
+import { Calculator, Plus, Divide, Minus, X, Sigma, Pi, Radical, Omega } from 'lucide-react'
 import { useEffect } from 'react'
 import MathModal from './math-popup'
+import myImage from './assets/qrcode.png';
 
 const Images = [
     [
-        "arrow.png",
-        "arrow.png"
+        
+        "q2.jpg",
+        "q12.jpg",
+        "q17.jpg",
+        "q21.jpg",
+        "q26.jpg",
+        "q30.jpg"
+        
     ],
     [
-        "arrow.png",
-        "arrow.png"
+        "q24.jpg",
+        "q9.jpg",
+        "q15.jpg",
+        "q14.jpg",
+        "q13.jpg",
+        "q32.jpg",
+        "q30.jpg"
+
+    ],
+    [
+        "q16.jpg",
+        "q29.jpg",
+        "q8.jpg",
+        "q31.jpg",
+        "q22.jpg",
+        "q1.jpg",
+    ],
+    [
+        "q33.jpg",
+        "q20.jpg",
+        "q5.jpg",
+        "q10.jpg",
+        "q7.jpg",
+
+    ],
+    [
+        "q4.jpg",
+        "q19.jpg",
+        "q3.jpg",
+        "q6.jpg",
+        "q18.jpg",
+    ],
+    [
+        "q11.jpg",
+        "q25.jpg",
+        "q30.jpg",
+        "q23.jpg",
+        "q27.jpg",
     ],
 ]
 
@@ -33,20 +76,35 @@ const MathNumberForm = () => {
     }
 
     return (
-        <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500">
+        <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 gap-14">
+            <div>
+                <img src={myImage} alt="" style={{ width: '330px', height: 'auto'}}/>
+            </div>
             <div className="absolute inset-0 overflow-hidden pointer-events-none">
                 <div className="absolute top-20 left-20 text-white/10 animate-float">
                     <Plus size={40} />
                 </div>
                 <div className="absolute top-40 right-32 text-white/10 animate-float-delayed">
-                    <Divide size={48} />
+                    <Divide size={44} />
                 </div>
                 <div className="absolute bottom-32 left-40 text-white/10 animate-float">
-                    <X size={36} />
+                    <X size={44} />
                 </div>
                 <div className="absolute bottom-20 right-20 text-white/10 animate-float-delayed">
                     <Minus size={44} />
                 </div>
+                <div className="absolute top-1/2 right-0 text-white/10 animate-float-delayed -translate-y-1/2">
+                    <Sigma size={40} />
+                </div>
+                <div className="absolute top-1/4 right-1/2 text-white/10 animate-float-delayed -translate-y-1/2">
+                    <Pi size={50} />
+                </div>         
+                <div className="absolute bottom-0 left-1/4 text-white/10 animate-float-delayed -translate-y-1/2">
+                    <Radical size={60} />
+                </div>     
+                <div className="absolute top-1/3 right-1/4 text-white/10 animate-float-delayed -translate-y-1/2">
+                    <Omega size={120} />
+                </div>      
             </div>
 
             <div className="w-[90%] md:w-full max-w-lg p-8 space-y-12 bg-white/10 backdrop-blur-lg rounded-2xl shadow-2xl border border-white/20">
--- src/math-popup.jsx
+++ src/math-popup.jsx
@@ -5,6 +5,7 @@ const MAX_SECOND = 60;
 
 const MathModal = ({ image, isOpen, onClose, setIsOpen, setImage }) => {
     const [timeLeft, setTimeLeft] = useState(MAX_SECOND)
+    const [backgroundColor, setBackgroundColor] = useState('white')
 
     const handleClose = useCallback(() => {
         setIsOpen(false)
@@ -20,6 +21,10 @@ const MathModal = ({ image, isOpen, onClose, setIsOpen, setImage }) => {
         document.body.style.overflow = 'hidden'
         
         setTimeLeft(MAX_SECOND)
+
+        
+        const randomChance = Math.random() * 100
+        setBackgroundColor(randomChance < 20 ? 'red' : 'white')
 
         const timer = setInterval(() => {
             setTimeLeft((prevTime) => {
@@ -41,7 +46,10 @@ const MathModal = ({ image, isOpen, onClose, setIsOpen, setImage }) => {
 
     return (
         <div className="fixed inset-0 z-50 flex items-center justify-center p-4 bg-black bg-opacity-50 backdrop-blur-sm">
-            <div className="relative w-full max-w-md bg-white rounded-2xl shadow-xl overflow-hidden p-2">
+            <div
+                className="relative w-full max-w-md rounded-2xl shadow-xl overflow-hidden p-2"
+                style={{ backgroundColor }}
+            >
                 <button
                     onClick={handleClose}
                     className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 rounded-full p-1"
